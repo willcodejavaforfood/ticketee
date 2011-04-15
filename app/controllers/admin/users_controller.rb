995f2a4f651e1faa85b485590e0cee47f20afdca
Erik Lundqvist <willcodejavaforfood@gmail.com>
2011-04-15 14:49:53 +0100

    Added the ability to create users through the admin gui

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,20 @@
 class Admin::UsersController < Admin::BaseController
   def index
+    @users = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.create(params[:user])
+    if @user.save
+      flash[:notice] = "User has been created."
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "User has not been created."
+      render :action => "new"
+    end
   end
 end
